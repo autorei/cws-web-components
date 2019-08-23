c6995be2c7bc9ddf4c5bc7df04ca53c2b82611d8
Willy Camargo <camargowilly@gmail.com>
2019-08-23 10:57:25 -0300

    Fix drone settings (#3)
    * fix: drone yaml file
    
    * update: docker image to use `tujoworker/docker-node-puppeteer`
    
    * update: drone docker image to use `tujoworker/docker-node-puppeteer`
    
    * update: stencil config to run browser tests without sandobox
    
    * update: eslint and prettier ignore files

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,19 +1,26 @@
-import { Config } from '@stencil/core';
+import { Config } from '@stencil/core'
 
 export const config: Config = {
   namespace: 'cws-web-components',
   globalStyle: 'src/global/index.css',
+  testing: {
+    /**
+     * Drone CI doesn't allow sandbox, therefor this parameters must be passed to your Headless Chrome
+     * before it can run your tests
+     */
+    browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader'
+      esmLoaderPath: '../loader',
     },
     {
-      type: 'docs-readme'
+      type: 'docs-readme',
     },
     {
       type: 'www',
-      serviceWorker: null // disable service workers
-    }
-  ]
-};
+      serviceWorker: null, // disable service workers
+    },
+  ],
+}
